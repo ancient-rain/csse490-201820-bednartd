c91b7887c7d8e6056824baa25f664a7d90870ec6
askalerion <bednartd@rose-hulman.edu>
2017-12-14 15:55:41 -0500

    Completed in class

--- JWT/JWTExample/models/user.js
+++ JWT/JWTExample/models/user.js
@@ -1,10 +1,24 @@
 // get an instance of mongoose and mongoose.Schema
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+    name: String,
+    salt: String,
+    hash: String,
+    admin: Boolean
+});
+
+userSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+}
+
+userSchema.methods.validPassword = function (password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+}
+
 // set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('User', new Schema({ 
-    name: String, 
-    password: String, 
-    admin: Boolean 
-}));
+module.exports = mongoose.model('User', userSchema);
--- JWT/JWTExample/server.js
+++ JWT/JWTExample/server.js
@@ -1,30 +1,32 @@
 // =======================
 // get the packages we need
 // =======================
-const express     = require('express');
-const app         = express();
-const bodyParser  = require('body-parser');
-const morgan      = require('morgan');
-const mongoose    = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const jwt    = require('jsonwebtoken');   // used to create, sign, 
+const jwt = require('jsonwebtoken'); // used to create, sign, 
 //  and verify tokens
-const config = require('./config');       // get our config file
-const User   = require('./models/user');  // get our mongoose model
-    
+const config = require('./config'); // get our config file
+const User = require('./models/user'); // get our mongoose model
+
 // =======================
 // configuration =========
 // =======================
-const port = process.env.PORT || 8080;    
+const port = process.env.PORT || 8080;
 mongoose.connect(config.database, {
     useMongoClient: true
 }); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // use morgan to log requests to the console
@@ -43,33 +45,129 @@ app.get('/', (req, res) => {
 app.get('/setup', (req, res) => {
 
     // create a sample user
+    const user = new User({
+        name: "Trevor Bednarek",
+        // password: "swordfish",
+        admin: true
+    });
+
+    user.setPassword('swordfish');
+
+    user.save(err => {
+        if (err) {
+            res.json(err);
+        } else {
+            console.log('User saved successfully!');
+            res.json({
+                success: true
+            });
+        }
+    });
 });
 
 // API ROUTES -------------------
 // we'll get to these in a second
 
-// TODO: 2 work on the apiRoutes 
+// DONE: 2 work on the apiRoutes 
+const apiRoutes = express.Router();
 
 // get an instance of the router for api routes
 
-// TODO: 4 route to authenticate a user 
-// (POST http://localhost:8080/api/authenticate)
+// DONE: 4 route to authenticate a user 
+// (POST http://localhost:8080/api/login)
 // Test in Postman
+apiRoutes.post('/login', (req, res) => {
+    User.findOne({
+        name: req.body.name
+    }, (err, user) => {
+        if (err) {
+            res.status(400);
+            res.json(err);
+        } else if (!user) {
+            res.status(404);
+            res.json({
+                success: false,
+                message: 'Authentication failed, no user.'
+            });
+        } else if (user) {
+            if (user.validPassword(req.body.password)) {
+                res.status(403);
+                res.json({
+                    success: false,
+                    message: 'Authentication failed, wrong password.'
+                });
+            } else {
+                const payload = {
+                    name: user.name,
+                    admin: user.admin
+                };
+                const token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: "24h"
+                });
+                res.json({
+                    success: true,
+                    message: 'Authentication successful!',
+                    token: token
+                });
+            }
+        }
+    });
+});
 
 // route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the coolest API on earth!'
+    });
+});
 
 // TODO: 5 route middleware to verify a token
 // route middleware to verify a token
+apiRoutes.use((req, res, next) => {
+    // look for token
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), (err, decoded) => {
+            if (err) {
+                res.status(403);
+                return res.json({
+                    success: false,
+                    message: 'Failed to authenticate token'
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        res.status(403);
+        return res.json({
+            success: false,
+            message: 'No token provided'
+        });
+    }
+});
 
 // route to return all users (GET http://localhost:8080/api/users)
 // Switch to Postman after TODO: 3 and have tested TODO: 1 in browser 
 
 
-// TODO: 3 apply the routes to our application with the prefix /api
+// DONE: 3 apply the routes to our application with the prefix /api
+apiRoutes.get('/users', (req, res) => {
+    User.find({}, '-password', (err, user) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(user);
+        }
+    });
+});
 
+app.use('/api', apiRoutes);
 
 // =======================
 // start the server ======
 // =======================
 app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
+console.log('Hosting at http://localhost:' + port);
