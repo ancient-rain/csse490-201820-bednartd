0a6f9f0688e496644a442d6600d9cde510b7cde9
askalerion <bednartd@rose-hulman.edu>
2018-01-09 13:58:04 -0500

    Added several test cases

--- BackendTesting/simple_tests/test/capitalize.js
+++ BackendTesting/simple_tests/test/capitalize.js
@@ -3,4 +3,24 @@ const capitalize = require("../capitalize");
 const chai = require("chai");
 const expect = chai.expect;
 
+const noWord = '';
+const noCap = 'test';
+const allCap = 'TEST';
+const firstLetterCap = 'Test';
+const secondLetterCap = 'tEst';
+const firstAndSecondLetterCap = 'TEst';
+const reverse = 'tEST';
 
+expect(capitalize(noWord)).to.equal('');
+
+expect(capitalize(noCap)).to.equal('Test');
+
+expect(capitalize(allCap)).to.equal('Test');
+
+expect(capitalize(firstLetterCap)).to.equal('Test');
+
+expect(capitalize(secondLetterCap)).to.equal('Test');
+
+expect(capitalize(firstAndSecondLetterCap)).to.equal('Test');
+
+expect(capitalize(reverse)).to.equal('Test');
