4aa9075c99f24fc076414e8669f694b94d7e0979
askalerion <bednartd@rose-hulman.edu>
2018-01-09 14:55:01 -0500

    Wrote supertests for html and js

--- BackendTesting/whats_my_useragent/test/html.js
+++ BackendTesting/whats_my_useragent/test/html.js
@@ -3,4 +3,34 @@ const app = require("../app");
 const supertest = require("supertest");
 const cheerio = require("cheerio");
 
+describe("html response", function () {
+    let request;
+    beforeEach(function () {
+        request = supertest(app)
+            .get("/")
+            .set("User-Agent", "my cool browser")
+            .set("Accept", "text/html");
+    });
 
+
+    it("returns a html response", function (done) {
+        request
+            .expect("Content-Type", /html/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("returns your User Agent", function (done) {
+        request
+            .expect(function (res) {
+                const htmlResponse = res.text;
+                const $ = cheerio.load(htmlResponse);
+                const userAgent = $('.user-agent').html().trim();
+
+                if (userAgent !== "my cool browser") {
+                    throw new Error("Response does not contain User Agent");
+                }
+            })
+            .end(done);
+    });
+});
