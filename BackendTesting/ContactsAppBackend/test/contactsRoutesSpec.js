5ffccda8bf0e4a7069122f37bff90b34f8270668
askalerion <bednartd@rose-hulman.edu>
2018-01-09 15:14:03 -0500

    Added contact route tests

--- BackendTesting/ContactsAppBackend/test/contactsRoutesSpec.js
+++ BackendTesting/ContactsAppBackend/test/contactsRoutesSpec.js
@@ -4,3 +4,28 @@ import supertest from 'supertest';
 
 import chai from 'chai';
 const should = chai.should();
+
+describe('contact route tests', function() {
+    let request;
+    beforeEach(function () {
+        request = supertest(app)
+            .get("/contacts")
+            .set("Accept", "application/json");
+    });
+
+    it("returns a json response", function (done) {
+        request
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(done);
+    });
+
+    it("returns array of contacts", function (done) {
+        request
+            .expect(function (res) {
+                let contacts = res.body;
+                Array.isArray(contacts).should.be.true;
+            })
+            .end(done);
+    });
+});
