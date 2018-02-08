159fedaa86d3cfeab037f02292b3b300c42eaaaa
Trevor Bednarek <bednartd@rose-hulman.edu>
2018-02-08 01:25:51 -0500

    Added basic form for generating a schedule

--- Lab5/client/src/app/app.module.ts
+++ Lab5/client/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { GenerateScheduleComponent } from './generate-schedule/generate-schedule.component';
 import { ViewScheduleComponent } from './view-schedule/view-schedule.component';
 
@@ -11,11 +16,16 @@ import { ViewScheduleComponent } from './view-schedule/view-schedule.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     GenerateScheduleComponent,
     ViewScheduleComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
--- Lab5/client/src/app/generate-schedule/generate-schedule.component.ts
+++ Lab5/client/src/app/generate-schedule/generate-schedule.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-generate-schedule',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateScheduleComponent implements OnInit {
 
-  constructor() { }
+  title: 'Generate a Schedule';
+  genSchedule: FormGroup;
+
+  constructor(fb: FormBuilder, private _router: Router) {
+    this.genSchedule = fb.group({
+      'sessionDays': new FormControl('', Validators.required),
+      'startDate': new FormControl('', Validators.required),
+      'startWeekNumber': new FormControl('', Validators.required),
+      'breakStartDate': new FormControl('', Validators.required),
+      'resumeDate': new FormControl('', Validators.required),
+      'numberOfSessions': new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  generate(value: any, valid: boolean) {
+    if (valid) {
+      console.log('Generating');
+      console.log('sessionDays', value.sessionDays);
+      console.log('startDate', value.startDate);
+      console.log('startWeekNumber', value.startWeekNumber);
+      console.log('breakStartDate', value.breakStartDate);
+      console.log('resumeDate', value.resumeDate);
+      console.log('numberOfSessions', value.numberOfSessions);
+    }
+    // this._router.navigate(['/session']);
   }
 
 }
