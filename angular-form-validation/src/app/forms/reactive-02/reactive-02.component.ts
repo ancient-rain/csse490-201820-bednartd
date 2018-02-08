af24779df8715754a3d3a27872004edad827a38d
askalerion <bednartd@rose-hulman.edu>
2018-02-08 02:58:02 -0500

    Adding solution

--- angular-form-validation/src/app/forms/reactive-02/reactive-02.component.ts
+++ angular-form-validation/src/app/forms/reactive-02/reactive-02.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+function ssnValidator(control: FormControl): { [key: string]: any } {
+  const value: string = control.value || '';
+  const valid = value.match(/^\d{9}$/);
+  return valid ? null : { ssn: true }; // null ⇒ no error
+}
+
+function equalValidator({ value }: FormGroup): { [key: string]: any } {
+  // get names of all properties in values object using destructuring
+  const [first, ...rest] = Object.keys(value || {});
+  // iterate through all the values and make sure they are equal
+  const valid = rest.every(v => value[v] === value[first]);
+  return valid ? null : { equal: true }; // null or an error object
+}
 
 @Component({
   selector: 'app-reactive-02',
@@ -9,16 +22,16 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class Reactive02Component implements OnInit {
 
-  formModel:  FormGroup;
+  formModel: FormGroup;
 
   constructor() {
-   this.formModel = new FormGroup({
-      'username': new FormControl(), // Example 1, predefined standard validator
-      'ssn': new FormControl(),
+    this.formModel = new FormGroup({
+      'username': new FormControl('', Validators.required), // Example 1, predefined standard validator
+      'ssn': new FormControl('', ssnValidator),
       'passwordsGroup': new FormGroup({
-        'password': new FormControl(),
-        'pconfirm': new FormControl()
-      })
+        'password': new FormControl('', [Validators.minLength(5), Validators.required]),
+        'pconfirm': new FormControl('')
+      }, equalValidator)
     });
   }
 
@@ -28,5 +41,9 @@ export class Reactive02Component implements OnInit {
 
   onSubmit() {
     console.log(this.formModel.value);
+  }
+
+  get password() {
+    return this.formModel.get('passwordsGroup.password');
   }
 }
