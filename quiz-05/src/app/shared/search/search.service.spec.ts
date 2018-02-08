6c7abf9149e417dececa1ebf24dc0943df8fdf8a
askalerion <bednartd@rose-hulman.edu>
2018-02-08 16:12:51 -0500

    Tried getting tests to work

--- quiz-05/src/app/shared/search/search.service.spec.ts
+++ quiz-05/src/app/shared/search/search.service.spec.ts
@@ -23,68 +23,80 @@ describe('SearchService', () => {
   }));
 
   it(`should retrieve all search results`,
-  async(inject([SearchService, HttpTestingController],
-    (searchService: SearchService, backend: HttpTestingController) => {
+    async(inject([SearchService, HttpTestingController],
+      (searchService: SearchService, backend: HttpTestingController) => {
 
-      // setup the mock object to use to determine response.
-      // In this case everything. You can make assertions
-      // or expectations against the expected results.
-      const mockResponse = [
-        {'name': 'John Elway'},
-        {'name': 'Gary Kubiak'}
-      ];
+        // setup the mock object to use to determine response.
+        // In this case everything. You can make assertions
+        // or expectations against the expected results.
+        const mockResponse = [
+          { 'name': 'John Elway' },
+          { 'name': 'Gary Kubiak' }
+        ];
 
-      // TODO: Add your test code here
-      expect(1).toBe(2);
+        // DONE: Add your test code here
+        // expect(1).toBe(2);
+        searchService.search('').subscribe(results => {
+          expect(results.name).toBeTruthy('John Elway');
+        });
 
-      // Except that a single request has been made that matches the given
-      // URL and flushes a mocked response
-      // TODO: Add Code here
-
-  })));
+        // Except that a single request has been made that matches the given
+        // URL and flushes a mocked response
+        // DONE: Add Code here
+        const request = backend.expectOne('John Elway');
+        request.flush(mockResponse, { status: 200, statusText: 'OK' });
+      })));
 
   it(`should filter by search term`,
-  async(inject([SearchService, HttpTestingController],
-    (searchService: SearchService, backend: HttpTestingController) => {
+    async(inject([SearchService, HttpTestingController],
+      (searchService: SearchService, backend: HttpTestingController) => {
 
-      // setup the mock object to use to determine response.
-      // In this case one of these objects. You can make assertions
-      // or expectations against the expected results.
-      const mockResponse = [
-        {'name': 'John Elway'},
-        {'name': 'Gary Kubiak'}
-      ];
+        // setup the mock object to use to determine response.
+        // In this case one of these objects. You can make assertions
+        // or expectations against the expected results.
+        const mockResponse = [
+          { 'name': 'John Elway' },
+          { 'name': 'Gary Kubiak' }
+        ];
 
 
-      // TODO: Add your test code here
-      expect(1).toBe(2);
+        // TODO: Add your test code here
+        // expect(1).toBe(2);
+        searchService.search('John').subscribe(results => {
+          expect(results.name).toBeTruthy('John Elway');
+        });
 
-      // Except that a single request has been made that matches the given
-      // URL and flushes a mocked response
-      // TODO: Add Code here
-
-  })));
+        // Except that a single request has been made that matches the given
+        // URL and flushes a mocked response
+        // TODO: Add Code here
+        const request = backend.expectOne('John Elway');
+        request.flush(mockResponse, { status: 200, statusText: 'OK' });
+      })));
 
 
   it(`should fetch by id`,
-  async(inject([SearchService, HttpTestingController],
-    (searchService: SearchService, backend: HttpTestingController) => {
+    async(inject([SearchService, HttpTestingController],
+      (searchService: SearchService, backend: HttpTestingController) => {
 
-      // setup the mock object to use to determine response.
-      // In this case one of these objects. You can make assertions
-      // or expectations against the expected results.
-      const mockResponse = [
-        {'id': 1, 'name': 'John Elway'},
-        {'id': 2, 'name': 'Gary Kubiak'}
-      ];
+        // setup the mock object to use to determine response.
+        // In this case one of these objects. You can make assertions
+        // or expectations against the expected results.
+        const mockResponse = [
+          { 'id': 1, 'name': 'John Elway' },
+          { 'id': 2, 'name': 'Gary Kubiak' }
+        ];
 
-      // TODO: Add your test code here
-      expect(1).toBe(2);
+        // TODO: Add your test code here
+        expect(1).toBe(2);
+        searchService.get(1).subscribe(results => {
+          expect(results.name).toBeTruthy('John Elway');
+        });
 
-      // Except that a single request has been made that matches the given
-      // URL and flushes a mocked response
-      // TODO: Add Code here
-
-  })));
+        // Except that a single request has been made that matches the given
+        // URL and flushes a mocked response
+        // TODO: Add Code here
+        const request = backend.expectOne('John Elway');
+        request.flush(mockResponse, { status: 200, statusText: 'OK' });
+      })));
 
 });
