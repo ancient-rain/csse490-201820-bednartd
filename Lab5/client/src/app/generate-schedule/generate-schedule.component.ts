8d059dbfdbc4f0faea44b5c4e683f399757e4e5c
Trevor Bednarek <bednartd@rose-hulman.edu>
2018-02-08 02:18:26 -0500

    Added validator for number of sessions

--- Lab5/client/src/app/generate-schedule/generate-schedule.component.ts
+++ Lab5/client/src/app/generate-schedule/generate-schedule.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+function numberOfSessionValidator(control: FormControl) {
+  const value: number = control.value || 1;
+  const valid = value > 0;
+  return valid ? null : {numberOfSessions: true};
+}
+
+function dateValidator() {
+
+}
+
 @Component({
   selector: 'app-generate-schedule',
   templateUrl: './generate-schedule.component.html',
@@ -11,15 +21,19 @@ export class GenerateScheduleComponent implements OnInit {
 
   title: 'Generate a Schedule';
   genSchedule: FormGroup;
+  sessionDays: ['M', 'T', 'W', 'TH', 'F', 'S'];
 
   constructor(fb: FormBuilder, private _router: Router) {
+
     this.genSchedule = fb.group({
       'sessionDays': new FormControl('', Validators.required),
-      'startDate': new FormControl('', Validators.required),
+      'numberOfSessions': new FormControl('', [Validators.required, numberOfSessionValidator]),
       'startWeekNumber': new FormControl('', Validators.required),
-      'breakStartDate': new FormControl('', Validators.required),
-      'resumeDate': new FormControl('', Validators.required),
-      'numberOfSessions': new FormControl('', Validators.required)
+      'datesGroup': fb.group({
+        'startDate': new FormControl('', Validators.required),
+        'breakStartDate': new FormControl('', Validators.required),
+        'resumeDate': new FormControl('', Validators.required)
+      })
     });
   }
 
